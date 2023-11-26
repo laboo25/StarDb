daf14d60c9d69dd4689fae40127dd20a49e9a9c1
lab00 <89362256+laboo25@users.noreply.github.com>
2023-11-26 12:58:10 +0600

    Update Preeloader.jsx

--- src/Preeloader.jsx
+++ src/Preeloader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/react';
-import { ClipLoader } from 'react-spinners';
+// import { ClipLoader } from 'react-spinners';
 
 const override = css`
   display: block;
