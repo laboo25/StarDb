6717a73614dca1a0a5074072aee6ee66f80eca76
lab00 <89362256+laboo25@users.noreply.github.com>
2023-11-26 13:06:12 +0600

    Update Preeloader.jsx

--- src/Preeloader.jsx
+++ src/Preeloader.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { css } from '@emotion/react';
+import React, { useState, useEffect } from 'react';
+// import { css } from '@emotion/react';
 // import { ClipLoader } from 'react-spinners';
 
-const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: red;
-`;
+const MyComponent = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // empty dependency array ensures that the effect runs only once
+// const override = css`
+//   display: block;
+//   margin: 0 auto;
+//   border-color: red;
+// `;
 
 const Preloader = () => {
   return (
@@ -15,6 +31,19 @@ const Preloader = () => {
   backgroundSize: "cover",
   backgroundPosition: "center"
 }}>
+      {isMobile ? (
+        <div className='w-full h-screen' style={{
+  backgroundImage: "url(https://raw.githubusercontent.com/laboo25/mag-imgs/main/index/sydneyCole.jpg)",
+  backgroundSize: "cover",
+  backgroundPosition: "center"
+}}></div>
+      ) : (
+        <div className='w-full h-screen' style={{
+  backgroundImage: "url(https://raw.githubusercontent.com/laboo25/mag-imgs/main/index/toriBlack.jpg)",
+  backgroundSize: "cover",
+  backgroundPosition: "center"
+}}></div>
+      )}
 {/*       <ClipLoader color="red" loading={true} css={override} size={150} /> */}
     </div>
   );
