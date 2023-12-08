c4145425dbac863278c4c86739fc94cebc76b418
lab00 <89362256+laboo25@users.noreply.github.com>
2023-12-09 02:32:08 +0600

    Update feetsData.jsx

--- src/data/feetsData.jsx
+++ src/data/feetsData.jsx
@@ -3,33 +3,51 @@ import { v4 as uuidv4 } from 'uuid';
 export const feetsdata = [
     {
         id: uuidv4(),
-        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/face―01.jpg",
-        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/face―01.jpg",
-        name: ['name4', 'name5'],
-    },
-    {
-        id: uuidv4(),
-        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/abellaDanger/face―04.jpg",
-        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/abellaDanger/face―04.webp",
-        name: ['name4', 'name2'],
-        aliases: ['name3', 'name2']
-    },
-    {
-        id: uuidv4(),
-        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/face―03.jpg",
-        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/face―03.jpg",
-        name: ['name4', 'name5'],
-    },
-    {
-        id: uuidv4(),
         imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―01.jpg",
-        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―05.jpg",
-        name: ['name4', 'name5'],
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―01%20mini.jpg",
+        name: ['lena anderson'],
+        aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
     },
     {
         id: uuidv4(),
-        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/face―04.jpg",
-        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/face―04.jpg",
-        name: ['name4', 'name5'],
+        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―02.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―02%20mini.jpg",
+        name: ['lena anderson'],
+        aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―03.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―03%20mini.jpg",
+        name: ['lena anderson'],
+        aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―04.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―04%20mini.jpg",
+        name: ['lena anderson'],
+        aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―05.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―05%20mini.jpg",
+        name: ['lena anderson'],
+        aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―06.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―06%20mini.jpg",
+        name: ['lena anderson'],
+        aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―07.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―07%20mini.jpg",
+        name: ['lena anderson'],
+        aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
     },
 ];
