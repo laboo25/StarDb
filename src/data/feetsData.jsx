22ac0f5046a6210029b3187631877710fc28fa27
lab00 <89362256+laboo25@users.noreply.github.com>
2023-12-09 04:10:43 +0600

    Update feetsData.jsx

--- src/data/feetsData.jsx
+++ src/data/feetsData.jsx
@@ -3,6 +3,263 @@ import { v4 as uuidv4 } from 'uuid';
 export const feetsdata = [
     {
         id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-1.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-1.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-2.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-2.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-3.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-3.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-4.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-4.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-5.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-5.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-6.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-6.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-7.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-7.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-8.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-8.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-9.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-9.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-10.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-10.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-11.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-11.webp",
+        name: ['zoe kravitz'],
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-12.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/zoe-kravitz/zoe-kravitz-12.webp",
+        name: ['zoe kravitz'],
+    },
+
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-1.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-1.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-2.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-2.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-3.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-3.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-4.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-4.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-5.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-5.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-6.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-6.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-7.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-7.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-8.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-8.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-9.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-9.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-10.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-10.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-11.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-11.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-12.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-12.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-13.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-13.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-14.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-14.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-15.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-15.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-16.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-16.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-17.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-17.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-18.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-18.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-19.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-19.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-20.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-20.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-21.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-21.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-22.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-22.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-23.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-23.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-24.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-24.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-25.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-25.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+    {
+        id: uuidv4(),
+        imgUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-26.jpg",
+        thumbnailUrl: "https://raw.githubusercontent.com/laboo25/feetImgs/main/ariel-grace/ariel-grace-26.webp",
+        name: ['ariel grace'],
+        // aliases: ['blaire ivory', 'jordan west', 'sloan kendricks']
+    },
+
+    
+    {
+        id: uuidv4(),
         imgUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―01.jpg",
         thumbnailUrl: "https://raw.githubusercontent.com/laboo25/mag-imgs/main/imgs/LenaAnderson/feet―01%20mini.jpg",
         name: ['lena anderson'],
