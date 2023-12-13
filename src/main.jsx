3328b732b71ebbf956e09b062cd0e93efad082a5
lab00 <89362256+laboo25@users.noreply.github.com>
2023-12-13 13:10:30 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -71,8 +71,6 @@ Fancybox.bind('[data-fancybox="gallery"]', {
       middle: [
         "zoomIn",
         "zoomOut",
-        "toggle1to1",
-        "rotateCCW",
         "rotateCW",
         "flipX",
         "flipY",
